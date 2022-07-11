c0afd9e993e6606460ac86c5779b55f245ca0801
saros1249 <saros1249@gmail.com>
2022-07-11 16:45:45 +0800

    Добавлен decorators

---
+++ service/decorators.py
@@ -0,0 +1,38 @@
+import jwt
+from flask import request, abort
+
+from constants import JWT_ALG, JWT_SECRET
+
+def auth_requered(func):
+    def wrapper(*args, **kwargs):
+        if not 'Authorization' in request.headers:
+            abort(401)
+
+        token = request.headers['Authorization']
+        try:
+            jwt.decode(token, JWT_SECRET, algorithm=[JWT_ALG])
+        except Exception as e:
+            print(f'JWT decode error: {e}')
+            abort(401)
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+def admin_requerd(func):
+    def wrapper(*args, **kwargs):
+        if not 'Authorization' in request.headers:
+            abort(401)
+
+        token = request.headers['Authorization']
+        try:
+            data = jwt.decode(token, JWT_SECRET, algorithm=[JWT_ALG])
+        except Exception as e:
+            print(f'JWT decode error: {e}')
+            abort(401)
+        else:
+            if data['role'] == 'admin'
+                return func(*args, **kwargs)
+        abort(403)
+
+    return wrapper
--- views/auth.py
+++ views/auth.py
@@ -1,7 +1,32 @@
+from flask import request
 from flask_restx import Namespace, Resource
+from implemented import auth_service
 
 auth_ns = Namespace('auth')
 
 @auth_ns.route('/')
 class AuthView(Resource):
     def post(self):
+        req_json = request.json
+        username = req_json.get('username')
+        password = req_json.get('password')
+        if not username or password:
+            return 'Не введены данные: имя пользователя и/или пароль.', 400
+
+        tokens = auth_service.generate_tokens(username, password)
+        if tokens:
+            return tokens
+        else:
+            return 'Ошибка запроса', 400
+
+    def put(self):
+        req_json = request.json
+        ref_token = req_json.get('refresh_token')
+        if not ref_token:
+            return 'Не задан токен', 400
+
+        tokens = auth_service.approve_refresh_token(ref_token)
+        if tokens:
+            return tokens
+        else:
+            return 'Ошибка запроса', 400
