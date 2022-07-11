4b533ed70f2ea075e9225f825b8c9da4eff0e788
saros1249 <saros1249@gmail.com>
2022-07-11 15:45:02 +0800

    Добавлен auth service

--- service/user.py
+++ service/user.py
@@ -16,6 +16,9 @@ class UserService():
     def get_all(self):
         return self.dao.get_all()
 
+    def get_by_username(self, username):
+        return self.dao.get_by_username(username)
+
     def create(self, user_d):
         return self.dao.create(user_d)
 
@@ -26,7 +29,7 @@ class UserService():
     def delete(self, uid):
         self.dao.delete(uid)
 
-    def make_user_password_hash(self, password):
+    def get_password_hash(self, password):
         return base64.b64encode(hashlib.pbkdf2_hmac(
             'sha256',
             password.encode('utf-8'),
@@ -34,9 +37,6 @@ class UserService():
             PWD_HASH_ITERATIONS
         ))
 
-    def compare_passwords(self, password_hash, other_password):
-        return hmac.compare_digest(
-            base64.b64decode(password_hash),
-            hashlib.pbkdf2_hmac('sha256', other_password.encode(), PWD_HASH_SALT, PWD_HASH_ITERATIONS)
-        )
+    def compare_passwords(self, password, password_hash):
+        return hmac.compare_digest(base64.b64decode(password_hash), base64.b64decode(self.get_password_hash(password)))
 
